65a9cf3c1fda6cef002f7868a9e276ba8e40c807
Ryan Groch <ryan.w.groch@gmail.com>
2026-01-06 14:06:57 -0600

    fix: cleans up temporary directories

--- makers/MakerAppImage.ts
+++ makers/MakerAppImage.ts
@@ -9,12 +9,17 @@ import {
   symlink,
   chmod,
   readFile,
+  rm,
 } from "fs/promises";
 import { tmpdir } from "os";
 import { resolve, relative } from "path";
 
 const RUNTIME_URL =
   "https://github.com/AppImage/type2-runtime/releases/download/continuous/runtime-x86_64";
+
+// For creating temporary work directories; largely arbitrary
+const APPDIR_PREFIX = "AppDir";
+const WORKDIR_PREFIX = "AppImageWorkDir";
 
 /**
  * Minimalist Forge maker for AppImages
@@ -54,68 +59,91 @@ export class MakerAppImage extends MakerBase<{}> {
 
     const runtime = await res.bytes();
 
-    // Create directory structure of AppDir.
-    // For conventions, see: https://docs.appimage.org/reference/appdir.html#conventions
-    const appDir = await mkdtemp(resolve(tmpdir(), exeName));
-    const binDir = resolve(appDir, "usr/bin");
-    const libDir = resolve(appDir, `usr/lib/${appName}`);
+    // Names of temporary directories to clean up later
+    let appDir: string | undefined;
+    let workDir: string | undefined;
 
-    await mkdir(binDir, { recursive: true, mode: 0o755 });
-    await mkdir(libDir, { recursive: true, mode: 0o755 });
+    try {
+      // Create directory structure of AppDir.
+      // For conventions, see: https://docs.appimage.org/reference/appdir.html#conventions
+      appDir = await mkdtemp(
+        resolve(tmpdir(), `${APPDIR_PREFIX}_${appName}_${version}_`),
+      );
+      const binDir = resolve(appDir, "usr/bin");
+      const libDir = resolve(appDir, `usr/lib/${appName}`);
 
-    // Add the actual application code to the AppDir
-    await cp(dir, libDir, { recursive: true });
+      await mkdir(binDir, { recursive: true, mode: 0o755 });
+      await mkdir(libDir, { recursive: true, mode: 0o755 });
 
-    // Generate .desktop file
-    // See: https://docs.appimage.org/reference/desktop-integration.html#desktop-files
-    // Also: https://specifications.freedesktop.org/desktop-entry/latest/recognized-keys.html
-    const desktopFile = `
-      [Desktop Entry]
-      Type=Application
-      Version=1.5
-      Name=${appName}
-      Exec=AppRun %U
-      X-AppImage-Name=${appName}
-      X-AppImage-Version=${version}
-      X-AppImage-Arch=x86_64
-    `
-      .replaceAll(/\n[ \t]+|[ \t]+\n/g, "\n") // Remove excess ws; only necessary due to string formatting aesthetics
-      .trim();
+      // Add the actual application code to the AppDir
+      await cp(dir, libDir, { recursive: true });
 
-    await writeFile(resolve(appDir, `${appName}.desktop`), desktopFile);
+      // Generate .desktop file
+      // See: https://docs.appimage.org/reference/desktop-integration.html#desktop-files
+      // Also: https://specifications.freedesktop.org/desktop-entry/latest/recognized-keys.html
+      const desktopFile = `
+        [Desktop Entry]
+        Type=Application
+        Version=1.5
+        Name=${appName}
+        Exec=AppRun %U
+        X-AppImage-Name=${appName}
+        X-AppImage-Version=${version}
+        X-AppImage-Arch=x86_64
+      `
+        .replaceAll(/\n[ \t]+|[ \t]+\n/g, "\n") // Remove excess ws; only necessary due to string formatting aesthetics
+        .trim();
 
-    // By convention, executables should be in /bin
-    await symlink(
-      relative(binDir, resolve(libDir, appName)),
-      resolve(binDir, appName),
-      "file",
-    );
+      await writeFile(resolve(appDir, `${appName}.desktop`), desktopFile);
 
-    // The entry point of an AppImage should be the AppRun file.
-    // See: https://docs.appimage.org/reference/appdir.html#general-description
-    await symlink(
-      relative(appDir, resolve(binDir, appName)),
-      resolve(appDir, "AppRun"),
-      "file",
-    );
+      // By convention, executables should be in /bin
+      await symlink(
+        relative(binDir, resolve(libDir, appName)),
+        resolve(binDir, appName),
+        "file",
+      );
 
-    // mksquashfs emits a file, so we create a temporary file
-    // inside a temporary directory to hold the output
-    const tempWorkDir = await mkdtemp(resolve(tmpdir(), "AppImageWorkDir"));
-    const tempSquashedFsPath = resolve(tempWorkDir, "temp");
-    execFileSync("mksquashfs", [appDir, tempSquashedFsPath]);
+      // The entry point of an AppImage should be the AppRun file.
+      // See: https://docs.appimage.org/reference/appdir.html#general-description
+      await symlink(
+        relative(appDir, resolve(binDir, appName)),
+        resolve(appDir, "AppRun"),
+        "file",
+      );
 
-    // Directory to hold final executable
-    await mkdir(outputDir, { recursive: true, mode: 0o755 });
+      // mksquashfs emits a file, so we create a temporary file
+      // inside a temporary directory to hold the output
+      workDir = await mkdtemp(
+        resolve(tmpdir(), `${WORKDIR_PREFIX}_${appName}_${version}_`),
+      );
+      const tempSquashedFsPath = resolve(workDir, "temp");
+      execFileSync("mksquashfs", [appDir, tempSquashedFsPath]);
 
-    // Per the documentation, AppImages should consist
-    // of the runtime prepended to the squashed fs.
-    // See: https://docs.appimage.org/reference/architecture.html
-    await writeFile(outputFilePath, runtime);
-    await appendFile(outputFilePath, await readFile(tempSquashedFsPath));
+      // Directory to hold final executable
+      await mkdir(outputDir, { recursive: true, mode: 0o755 });
 
-    await chmod(outputFilePath, 0o755);
+      // Per the documentation, AppImages should consist
+      // of the runtime prepended to the squashed fs.
+      // See: https://docs.appimage.org/reference/architecture.html
+      await writeFile(outputFilePath, runtime);
+      await appendFile(outputFilePath, await readFile(tempSquashedFsPath));
 
-    return [outputFilePath];
+      await chmod(outputFilePath, 0o755);
+
+      return [outputFilePath];
+    } finally {
+      // Clean up temporary directories
+      if (appDir)
+        await rm(appDir, {
+          recursive: true,
+          force: true,
+        });
+
+      if (workDir)
+        await rm(workDir, {
+          recursive: true,
+          force: true,
+        });
+    }
   }
 }
